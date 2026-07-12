342f410d89385814eddc6426d84422407bab13c6
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-07-12 19:22:37 +0530

    added document_similarity.py

---
+++ Embedding_Models/document_similarity.py
@@ -0,0 +1,30 @@
+from langchain_huggingface import HuggingFaceEmbeddings
+from dotenv import load_dotenv
+from sklearn.metrics.pairwise import cosine_similarity
+
+
+load_dotenv()
+
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+
+documents = [
+    "Lionel Messi is a Goat..",
+    "Pelé, also known as the 'God of Football', holds many goal-scoring records..",
+    "Kylian Mbappé is known for his elegant dribbling and record-breaking hat-tricks.",
+    "Jude Bellingham is an England midfielder famous for his calm demeanor and finishing skills.",
+    "Erling Haaland is a Norwegian striker known for his unorthodox movement and power."
+]
+
+query = "Who is Lionel Messi?"
+
+query_vector = embeddings.embed_query(query)
+vectors = embeddings.embed_documents(documents)
+
+
+similarities = cosine_similarity([query_vector], vectors)
+
+index, score = sorted(list(enumerate(similarities[0])), key=lambda x: x[1], reverse=True)[0]
+
+print(query)
+print(documents[index])
+print("similarity score:", score)
