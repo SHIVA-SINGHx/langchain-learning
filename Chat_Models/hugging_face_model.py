3546df70e286dc8ffa0cd4c245edddeea2e576f3
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-07-12 18:39:25 +0530

    hugging_face_model.py added

---
+++ Chat_Models/hugging_face_model.py
@@ -0,0 +1,26 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="conversational",
+    huggingfacehub_api_token=os.getenv(
+        "HUGGINGFACEHUB_API_TOKEN"
+    )
+)
+
+
+model = ChatHuggingFace(llm=llm)
+
+
+response = model.invoke(
+    "What is the capital of UK?"
+)
+
+
+print(response.content)
