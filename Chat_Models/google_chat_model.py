cd33b33df8007e79ad954b4cd1cd518eb6bebebd
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-07-12 17:46:59 +0530

    initial-commit

---
+++ Chat_Models/google_chat_model.py
@@ -0,0 +1,11 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatGoogleGenerativeAI(model="gemini-3-flash-preview", temperature=0.8)
+
+
+result = model.invoke("What is the capital of France?")
+
+print(result.text)
