b9ef52a9345b4d490c267da1d680a03b6a680539
SHIVA-SINGHx <shivasingh.1dev@gmail.com>
2026-07-18 18:04:57 +0530

    Added output_parser

---
+++ Output_Parsers/stroutputparser.py
@@ -0,0 +1,40 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="meta-llama/Llama-3.1-8B-Instruct",
+    task="conversational",
+    huggingfacehub_api_token=os.getenv(
+        "HUGGINGFACEHUB_API_TOKEN"
+    )
+)
+
+model = ChatHuggingFace(llm=llm)
+
+#template-1 
+template1 = PromptTemplate(
+    template = "write a detailed report on {topic}",
+    input_variables= ['topic']
+)
+
+# tempalte-2
+template2 = PromptTemplate(
+    template = "Write a 5 line of summary on a following text. /n {text}",
+    input_variables= ['text']
+)
+
+prompt1 = template1.invoke({'topic': 'albert camus'})
+
+result = model.invoke(prompt1)
+
+prompt2 = template2.invoke({'text': result.content})
+
+res = model.invoke(prompt2)
+
+print(res.content)
